f017816eb18bd8ac3846fd0d24c53b6e68817a48
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2026-08-05 16:12:23 +0900

    구조체 패키지 분리
    worker 내부 구조체 -> model 패키지의 구조체로 변경

---
+++ judge_server/internal/model/job.go
@@ -0,0 +1,8 @@
+package model
+
+//row data from user
+type Job struct {
+	ID       string
+	Language string
+	Source   string
+}
--- judge_server/internal/worker/worker.go
+++ judge_server/internal/worker/worker.go
@@ -1,6 +1,10 @@
 package worker
 
-import "fmt"
+import (
+	"fmt"
+
+	"judge_server/internal/model"
+)
 
 type Worker struct {
 }
@@ -11,6 +15,26 @@ func New() *Worker {
 
 func (w *Worker) Run() error {
 	fmt.Println("Worker is running")
+	var testID string = "12"
+	testLanguage := "Python"
+	testSource :=
+		`print("hello, world!")
+if 1:
+	print("1")
+`
+
+	w.Process(model.Job{
+		ID:       testID,
+		Language: testLanguage,
+		Source:   testSource,
+	})
+	return nil
+}
+
+func (w *Worker) Process(submission model.Job) error {
+	fmt.Println(submission.ID)
+	fmt.Println(submission.Language)
+	fmt.Println(submission.Source)
 
 	return nil
 }
